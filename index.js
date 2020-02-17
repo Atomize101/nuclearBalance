0f3325d98da52835b2ada8b0a9bde0603dc4cd70
Chris Patrick <atomize101@gmail.com>
2020-02-17 11:46:27 -0500

    Setting up some html inputs

--- index.js
+++ index.js
@@ -3,9 +3,21 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('hi there');
+    res.send(`
+        <div>
+            <form>
+                <input placeholder="email" />
+                <input placeholder="password" />
+                <input placeholder="password confirmation" />
+                <button>Sign Up</button>
+            </form>
+        </div>
+    `);
 });
 
+app.get('/boo', (req, res) => {
+    res.send('hi boo');
+});
 
 app.listen(3000, () => {
     console.log('Server online');

90f0fb898df8a0b56f2918f1411769bb77461859
Chris Patrick <atomize101@gmail.com>
2020-02-19 11:47:36 -0500

    getOneBy method added

--- repositories/users.js
+++ repositories/users.js
@@ -46,24 +46,41 @@ class UserRepository {
         const filteredRecords = records.filter(record => record.id !== id);
         await this.writeAll(filteredRecords);
     }
-    
+
     async update(id, attrs) {
         const records = await this.getAll();
         const record = records.find(record => record.id === id);
 
-        if(!record) {
+        if (!record) {
             throw new Error(`Record with id ${id} not found`)
         }
         Object.assign(record, attrs);
         await this.writeAll(records);
     }
 
+    async getOneBy(filters) {
+        const records = await this.getAll();
+
+        for (let record of records) {
+            let found = true;
+
+            for (let key in filters) {
+                if (record[key] !== filters[key]) {
+                    found = false;
+                }
+            }
+            if (found) {
+                return record;
+            }
+        }
+    }
 }
 
 const test = async () => {
 
     const repo = new UserRepository('users.json');
-    await repo.update('4cb64efb', { password: 'blah'});
+    const user = await repo.getOneBy({ id: 'test@test.com'});
+    console.log(user);
 }
 
 test();

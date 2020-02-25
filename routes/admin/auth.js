9fecc0cca298e4b8de07b6dee3f9376429fff636
Chris Patrick <atomize101@gmail.com>
2020-02-25 16:25:38 -0500

    Testing validators

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { check } = require('express-validator');
+const { check, validationResult } = require('express-validator');
 
 const usersRepo = require('../../repositories/users');
 const signupTemplate = require('../../views/admin/auth/signup');
@@ -11,11 +11,17 @@ router.get('/signup', (req, res) => {
     res.send(signupTemplate({ req }));
 });
 
-router.post('/signup', [
-    check('email'),
-    check('password'),
-    check('passwordConfirmation')
-], async (req, res) => {
+router.post('/signup', 
+[
+    check('email').trim().normalizeEmail().isEmail(),
+    check('password').trim().isLength({ min: 4, max: 20 }),
+    check('passwordConfirmation').trim().isLength({ min: 4, max: 20 })
+], 
+
+async (req, res) => {
+
+    const errors = validationResult(req);
+    console.log(errors);
     const { email, password, passwordConfirmation } = req.body;
     const existingUser = await usersRepo.getOneBy({ email });
     if (existingUser) {
@@ -48,15 +54,15 @@ router.get('/signin', (req, res) => {
 router.post('/signin', async (req, res) => {
     const { email, password } = req.body;
     const user = await usersRepo.getOneBy({ email });
-    
-    if(!user) {
+
+    if (!user) {
         return res.send('Email not found');
     }
     const validPassword = await usersRepo.comparePasswords(
         user.password,
         password
     );
-    if(!validPassword) {
+    if (!validPassword) {
         return res.send('Invalid Password');
     }
 
